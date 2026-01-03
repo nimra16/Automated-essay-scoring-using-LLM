1a2aae9cb0e08d46f8e151770520c91e19414377
nimra16 <nimra.mscsf19@iba-suk.edu.pk>
2026-01-03 17:42:11 +0500

    Updated experiments and results

---
+++ 2_learning-agency-lab-automated-essay-scoring-2/Demo.java
@@ -0,0 +1,11 @@
+class Demo{
+    public static void main(String a[]){
+        int[] ar[] = new int[5][5];
+        for (int[]ab: ar){
+            for (int b: ab){
+                System.out.println(b);
+            }
+        }
+        
+    }
+}
